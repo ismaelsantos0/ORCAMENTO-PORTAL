2796f330557c6f6057e4be5276fb53e3d878fd0c
Ismael Santos <ismaelifrr@gmail.com>
2026-02-12 18:16:08 -0400

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -1,4 +1,7 @@
 import math
 
-def ceil_div(a, b):
-    return int(math.ceil(a / b))
+def ceil_div(a: float, b: float) -> int:
+    """Ceil(a/b) com segurança."""
+    if b == 0:
+        return 0
+    return int(math.ceil(float(a) / float(b)))
